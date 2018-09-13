308eabd9bbd750f0077656457f14f45c46f49d4b
MZI <macchenjl@foxmail.com>
2018-09-13 22:34:22 +0800

    Transfer the haruno-plugin-turing

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -1,11 +1,6 @@
 package plugins
 
-import (
-	"github.com/haruno-bot/haruno/coolq"
-	"github.com/haruno-bot/haruno/plugins/turing"
-)
-
 // SetupPlugins 安装插件的入口
 func SetupPlugins() {
-	coolq.PluginRegister(turing.Instance)
+	// coolq.PluginRegister(plugin.Instance)
 }
--- plugins/turing/type.go
+++
@@ -1,7 +0,0 @@
-package turing
-
-// Answer 图灵机器人返回格式
-type Answer struct {
-	Code int    `json:"code"`
-	Text string `json:"text"`
-}
